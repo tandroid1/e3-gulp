9758765f0b68bc4573380cb5f59caf978f0ed39c
Tanner Langley <tlangley141@gmail.com>
2015-07-01 16:42:13 -0600

    Basic gulpfile.

--- site/sites/all/themes/e3_zen/gulpfile.js
+++ site/sites/all/themes/e3_zen/gulpfile.js
@@ -2,20 +2,9 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var compass = require('gulp-compass');
-var livereload = require('gulp-livereload');
 
 gulp.task('sass', function() {
   gulp.src('./sass/*.scss')
-    .pipe(compass({
-      config_file: './config.rb',
-      css: 'css',
-      sass: 'sass'
-    }))
-    .pipe(livereload());
-});
-
-gulp.task('watch', function() {
-  livereload.listen();
-  gulp.watch('./sass/**/*.scss', ['sass']);
+    .pipe(sass())
+    .pipe(gulp.dest('./css'));
 });
