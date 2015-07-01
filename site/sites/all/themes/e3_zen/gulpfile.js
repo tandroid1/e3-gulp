d182b31864396dcf8a6cf16d940c009a52554cdf
Tanner Langley <tlangley141@gmail.com>
2015-07-01 16:44:26 -0600

    Add compass

--- site/sites/all/themes/e3_zen/gulpfile.js
+++ site/sites/all/themes/e3_zen/gulpfile.js
@@ -2,9 +2,17 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var compass = require('gulp-compass');
 
 gulp.task('sass', function() {
   gulp.src('./sass/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('./css'));
+    .pipe(compass({
+      config_file: './config.rb',
+      css: 'css',
+      sass: 'sass'
+    }));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./sass/**/*.scss', ['sass']);
 });
